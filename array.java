73f815d12e1d8dec4307323dbdb643a98d96a6b5
harshit <iamharshitsharma518@gmail.com>
2025-06-27 00:37:55 +0530

    first commit

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array{
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = {1, 2, 3, 4, 5};   
+        // Print the elements of the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }   
+    }
+}
