7cbcd847bd60c2f4ec0ef3eb4e026d0275cf0035
harshit <iamharshitsharma518@gmail.com>
2025-06-29 21:21:13 +0530

    first part done make reverse function

---
+++ rotatearr.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class rotatearr {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        reverse(arr,0,n-1);
+        print(arr);
+
+    }
+    public static void reverse(int arr[],int s,int e){
+         while(s<=e){
+             int temp = arr[s];
+             arr[s]=arr[e];
+             arr[e]=temp;
+             s++;
+             e--;
+         }
+
+    }
+ public static void print(int arr[]){
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i] + " ");
+    }
+ }
+
+
+}
