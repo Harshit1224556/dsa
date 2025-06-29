40e5bece0a1f39f1ffc23aa95ddccdb5c9aab7ab
harshit <iamharshitsharma518@gmail.com>
2025-06-29 21:29:02 +0530

    final code

--- rotatearr.java
+++ rotatearr.java
@@ -7,8 +7,12 @@ public class rotatearr {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-
+        int k = sc.nextInt();
+        reverse(arr,0,n-k-1);
+        reverse(arr,n-k,n-1);
         reverse(arr,0,n-1);
+
+
         print(arr);
 
     }
