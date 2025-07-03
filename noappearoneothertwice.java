b4180c12f2fc9fd792b78fd4d706e1babb38672f
harshit <iamharshitsharma518@gmail.com>
2025-07-03 21:36:10 +0530

    question done

---
+++ noappearoneothertwice.java
@@ -0,0 +1,8 @@
+public class noappearoneothertwice {
+    public static void main(String[] args) {
+        
+        //input - 1 1 2 2 3
+        //output -3 
+        //everyone other no appear twice except this 3 thats appear only once 
+    }
+}
