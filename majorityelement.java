59a94cf89769391a19043a0556bc18b4d99ce7df
harshit <iamharshitsharma518@gmail.com>
2025-07-11 09:24:17 +0530

    kadane algorithm clear

---
+++ majorityelement.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class majorityelement {
+    public static void main(String[] args) {
+        
+
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      //enter the elements of the array
+      int arr[] = new int[n];
+
+      for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+      }
+
+          int c= 0;
+      for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(arr[i]==arr[j]) c++;
+            if(c>(n/2)) {
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+      }
+    }
+}
