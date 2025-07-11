b7077e87534c928e8cb81a73ba9be85788a7f2ce
harshit <iamharshitsharma518@gmail.com>
2025-07-11 11:42:34 +0530

    linkedlist

---
+++ linkedlist/basicll.java
@@ -0,0 +1,29 @@
+
+public class basicll {
+    public static void main(String[] args) {
+        
+        class Node{
+            int data;
+            Node next;
+            Node(int data){
+                this.data = data;
+            }
+        }
+
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+
+        System.out.println(a.data);
+        System.out.println(a.next.data);
+        System.out.println(a.next.next.data);
+        System.out.println(a.next.next.next.data);
+        System.out.println(a.next.next.next.next.data);
+    }
+}
