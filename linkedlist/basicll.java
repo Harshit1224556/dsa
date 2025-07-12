a90e2fa50aa87339b519187580bd9c2fa21ed06a
harshit <iamharshitsharma518@gmail.com>
2025-07-12 11:14:38 +0530

    display

--- linkedlist/basicll.java
+++ linkedlist/basicll.java
@@ -1,8 +1,19 @@
 
 public class basicll {
-    public static void main(String[] args) {
-        
-        class Node{
+
+
+    public static void display(Node head){
+
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+
+    }
+
+
+       public static class Node{
             int data;
             Node next;
             Node(int data){
@@ -10,6 +21,7 @@ public class basicll {
             }
         }
 
+    public static void main(String[] args) {
         Node a = new Node(10);
         Node b = new Node(20);
         Node c = new Node(30);
@@ -28,10 +40,15 @@ public class basicll {
 
         //display a linkedlist using loop rather than printing manually only head is given with you 
 
-        while(a!=null){
-            System.out.println(a.data);
-            a=a.next;
-        }
+        // while(a!=null){
+        //     System.out.println(a.data);
+        //     a=a.next;
+        // }
 
+
+        //lets do it by making functions
+
+        //just make a function and what we have to do just track it by 
+        display(a);
     }
 }
