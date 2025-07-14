c3849332f812d0f2f02b6a4990bfd0bdc0c565a5
harshit <iamharshitsharma518@gmail.com>
2025-07-14 10:58:54 +0530

    optimal approach done

--- array/twosum2.java
+++ array/twosum2.java
@@ -20,12 +20,32 @@ public class twosum2 {
         //brute force approach
         int count[] = new int[2];
         int n = arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]+arr[j]==target){
-                    count[0]=i+1;
-                    count[1]=j+1;
-                }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[i]+arr[j]==target){
+        //             count[0]=i+1;
+        //             count[1]=j+1;
+        //         }
+        //     }
+        // }
+
+
+        //optimal approach
+
+        int l = 0;
+        int r = n-1;
+
+        while(l<r){
+            if((arr[l]+arr[r])>target){
+                r--;
+            }
+            else if((arr[l]+arr[r])==target){
+               count[0]=l+1;
+               count[1]=r+1;
+               break;
+            }
+            else{
+                l++;
             }
         }
 
