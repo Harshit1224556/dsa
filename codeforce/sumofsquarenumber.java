e6995ba792c10c9f6a8201e13c40161f2619f82b
harshit <iamharshitsharma518@gmail.com>
2025-09-09 00:32:25 +0530

    jsut done with brute force approach

---
+++ codeforce/sumofsquarenumber.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class sumofsquarenumber {
+    
+    public static void main(String[] args) {
+        
+
+Scanner sc  = new Scanner(System.in);
+int n = sc.nextInt();
+
+
+System.out.println(istriplet(n));
+
+
+
+    }
+
+    public static boolean  istriplet(int n){
+
+
+
+// int low = 0;
+// int high = n/2;
+
+// for(int i=low;i<=high;i++)
+// {
+//     for(int j=i+1;j<=high;j++){
+//         if(((i*i) + (j*j))==n) return true;
+
+        
+//     }
+// }
+
+
+// return false;
+
+
+//thats may be the brute force approach now lets move to the optimal approach
+
+
+        
+    }
+}
