c00d132532f5d24cc6baa7011d9cd4399c6fca92
harshit <iamharshitsharma518@gmail.com>
2025-09-08 18:53:46 +0530

    lets get hooke up with code force

--- array/minimumboat.java
+++ array/minimumboat.java
@@ -2,4 +2,5 @@ package array;
 
 public class minimumboat {
     
+    
 }
--- recurssion/print1ton.java
+++ recurssion/print1ton.java
@@ -5,15 +5,21 @@ public class print1ton {
     public static void main(String[] args) {
 Scanner sc =  new Scanner(System.in);
 int n = sc.nextInt();
-print(n);
+print(5,1);
+
 
 
     }
 
-    public static void print(int n){
+    public static void print(int n,int start){
             
-        if(n==0) return;
-        System.out.println(n);
-        print(n-1);
+        System.out.println(start);
+        if(n==start) return;
+        print(n,start+1);
+        
     }
 }
+
+//firsly done n to 1
+
+//firstly print 1 to n
