155beb88d19ced9c6653af0fca9424711b7c80a9
harshit <iamharshitsharma518@gmail.com>
2025-09-09 22:03:38 +0530

    using loop

---
+++ recurssion/fibon.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  
+
+
+int i =1;
+int x = 1;
+int y = 0;
+int z = 1;
+while(i<=5){
+    cout << z;
+    x=y;
+    y=z;
+    z=x+y;
+    i++;
+}
+
+
+
+    return 0;
+}
