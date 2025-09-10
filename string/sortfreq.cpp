4662300474f699a9abc586a3395b9366335a0b72
harshit <iamharshitsharma518@gmail.com>
2025-09-10 23:22:46 +0530

    thats the correct solution of the code

--- string/sortfreq.cpp
+++ string/sortfreq.cpp
@@ -10,12 +10,35 @@ for(char c:s){
     freq[c]++;
 }
 string result = "";
-for(auto it:freq){
-    for(int i=1;i<=it.second;i++){
-        result+=it.first;
+
+    // for(int i=1;i<=it.second;i++){
+    //     result+=it.first;
+    // }
+
+    while(!freq.empty()){
+        char maxChar;
+        int maxfreq = 0;
+        for(auto it:freq){
+            if(it.second>maxfreq){
+                
+                maxChar=it.first;
+                maxfreq= it.second;
+            }
+        }
+        for(int i=0;i<maxfreq;i++){
+
+            result+=maxChar;
+        }
+        freq.erase(maxChar);
+
     }
-}
+
+    
+
 cout << result;
 
     return 0;
 }
+
+
+
