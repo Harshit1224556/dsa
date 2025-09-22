344ccd4a3776ee7df11de0dbe0fdebe522170407
harshit <iamharshitsharma518@gmail.com>
2025-09-22 20:25:42 +0530

    just done with the subset

---
+++ recurssion/subsetofstring.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printsubset(string res,string s,int ind){
+    if(ind==s.length()){
+        cout << res << endl;
+        return;
+    }
+    char  ch = s[ind];
+    printsubset(res+ch,s,ind+1);
+    printsubset(res,s,ind+1);
+}
+int main()
+{
+
+    string s;
+    cin>>s;
+    printsubset("",s,0);
+    return 0;
+}
