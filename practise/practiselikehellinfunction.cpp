ac80254fe2b04f4d05f75c7d6b54dc2ad8ce07b1
harshit <iamharshitsharma518@gmail.com>
2025-10-09 19:52:42 +0530

    practise some pattern pritning

---
+++ practise/practiselikehellinfunction.cpp
@@ -0,0 +1,54 @@
+//1 
+//power ka function banao
+//two paramenter leta hah 
+//and return pow
+
+
+
+//Write a C++ program to print all Perfect numbers between 1 to n
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n){
+    for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        cout<<"*";
+
+    }
+    cout << endl;
+}
+
+}
+int main()
+{
+
+
+//pattern printing
+
+
+int n;
+cin>>n;//5
+pattern1(n);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
